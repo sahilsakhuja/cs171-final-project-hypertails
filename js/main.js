b0ffc702b669e2d1d4b2c26736c5fdf169e8f286
jlp2501 <lyp245@g.harvard.edu>
2021-11-14 05:48:58 -0500

    first top5 viz

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-
+//import {topHyperVis} from "./top5hyperVis";
 
 let worldVis;
 let top5hyperVis;
+let top5IndexVis;
 let commodityVis;
 let increaseVis;
+
+
+let initialData = null;
 
 // Margin object with properties for the four directions
 let margin = {top: 20, right: 10, bottom: 20, left: 10};
@@ -17,10 +21,67 @@ let plot_width = 700 - margin.left - margin.right,
   plot_height = 500 - margin.top - margin.bottom;
 
 // Define 'svg' as a child-element (g) from the drawing area and include spaces
-let svg = d3.select("#chart-area").append("svg")
+/*let svg = d3.select("#top5hyper").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");*/
 
 let padding = 30;
+
+// load data using promises
+
+let promises = [
+
+    d3.csv("data/Top5_hyperinflation.csv"),
+    /*d3.csv("data/IMF_Angola_index.csv"),
+    d3.csv("data/IMF_Bulgaria_index.csv"),
+    d3.csv("data/IMF_Sudan_index.csv"),
+    d3.csv("data/IMF_Turkey_index.csv"),
+    d3.csv("data/IMF_Zimbabwe_index.csv")*/
+    d3.csv("data/IMF_all_index.csv")
+
+];
+
+
+Promise.all(promises)
+    .then(function (data) {
+        initialData = data;
+        initMainPage(data)
+    })
+    .catch(function (err) {
+        console.log(err)
+    });
+
+
+function initMainPage(dataArray) {
+    console.log(dataArray[0])
+
+    top5hyperVis = new topHyperVis ('top5hyper', dataArray[0])
+    top5IndexVis = new topIndexVis ('top5index', dataArray[1])
+    console.log(dataArray[1])
+
+}
+
+
+let selectedCategory = document.getElementById('categorySelector').value;
+function categoryChange() {
+    selectedCategory = document.getElementById('categorySelector').value;
+    /*myMapVis.wrangleData(); // maybe you need to change this slightly depending on the name of your MapVis instance
+    myBarVisOne.wrangleData();
+    myBarVisTwo.wrangleData();
+    myBrushVis.wrangleData();*/
+}
+
+let selectedCategoryEl = document.getElementById('categorySelector');
+selectedCategoryEl.addEventListener('change', function (event) {
+    // initMainPage(initialData);
+    //top5hyperVis.line.exit().remove();
+    let paths = document.querySelectorAll("path");
+    let lastPath = paths[paths.length - 1];
+    lastPath.style.display = "none";
+
+    top5hyperVis.updateVis();
+})
+
+
---
+++ js/top5indexVis.js
@@ -0,0 +1,171 @@
+/*class topIndexVis {
+
+    constructor(parentElement, countryindex) {
+        this.parentElement = parentElement;
+        this.countryindex = countryindex;
+
+        this.initVis()
+    }
+
+    initVis(){
+
+        let vis = this;
+
+        vis.margin = {top: 20, right: 20, bottom: 20, left: 40};
+        //vis.width = 500;
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width+200 - vis.margin.left - vis.margin.right;
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+
+
+        // init drawing area
+        vis.svg = d3.select("#" + vis.parentElement).append("svg")
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .append('g')
+            .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
+
+
+        vis.x = d3.scaleTime()
+            .range([0, vis.width]);
+
+        vis.y = d3.scaleLinear()
+            .range([vis.height, 0]);
+
+        vis.xAxis = d3.axisBottom()
+            .scale(vis.x);
+
+        vis.yAxis = d3.axisLeft()
+            .scale(vis.y);
+
+        vis.svg.append("g")
+            .attr("class", "x-axis axis")
+            .attr("transform", "translate(0," + vis.height + ")");
+
+        vis.svg.append("g")
+            .attr("class", "y-axis axis");
+
+
+
+        this.wrangleData();
+
+    }
+
+    wrangleData(){
+        let vis = this;
+
+        let filteredIndexData = [];
+
+        let parseTime = d3.timeParse("%m/%d/%Y");
+
+        for (let item of vis.countryindex) {
+            let year = parseTime(item["Year"]);
+            let country = item['Country'];
+            let cpi_all = parseFloat(parseFloat(item['Consumer Price Index, All items']).toFixed(2));
+            let food_beverages = parseFloat(parseFloat(item['Food and non-alcoholic beverages']).toFixed(2));
+            let alcohol_tobacco = parseFloat(parseFloat(item['Alcoholic Beverages, Tobacco, and Narcotics']).toFixed(2));
+            let clothing_footwear = parseFloat(parseFloat(item['Clothing and footwear']).toFixed(2));
+            let housing_electric_gas = parseFloat(parseFloat(item['Housing, Water, Electricity, Gas and Other Fuels']).toFixed(2));
+            let household_equipment = parseFloat(parseFloat(item['Furnishings, household equipment and routine household maintenance']).toFixed(2));
+            let health = parseFloat(parseFloat(item['Health']).toFixed(2));
+            let communication = parseFloat(parseFloat(item['Communication']).toFixed(2));
+            let transport = parseFloat(parseFloat(item['Transport']).toFixed(2));
+            let recreation = parseFloat(parseFloat(item['Recreation and culture']).toFixed(2));
+            let education = parseFloat(parseFloat(item['Education']).toFixed(2));
+            let restaurant = parseFloat(parseFloat(item['Restaurants and hotels']).toFixed(2));
+            let misc = parseFloat(parseFloat(item['Miscellaneous goods and services']).toFixed(2));
+
+
+
+            let newItem = {
+                "year": year,
+                "country": country,
+                "cpi_all": cpi_all,
+                "food_beverages": food_beverages,
+                "alcohol_tobacco": alcohol_tobacco,
+                "clothing_footwear": clothing_footwear,
+                "housing_electric_gas": housing_electric_gas,
+                "household_equipment": household_equipment,
+                "health": health,
+                "communication": communication,
+                "transport": transport,
+                "recreation": recreation,
+                "education": education,
+                "restaurant": restaurant,
+                "misc": misc
+
+
+            }
+            filteredIndexData.push(newItem)
+        };
+        console.log(filteredIndexData)
+
+        vis.filteredIndex = filteredIndexData;
+
+
+
+        vis.updateVis()
+
+    }
+
+    updateVis(){
+
+
+        let selectedCategoryEl = document.getElementById("categorySelector");
+        let selectedCategory = selectedCategoryEl.value;
+
+        let vis = this;
+
+
+        // (1) Update domain
+        vis.x.domain(d3.extent(vis.filteredIndex.map(function (d){
+            return d.year;
+        })))
+
+        vis.y.domain(d3.extent(vis.filteredIndex.map(function (d) {
+            return d[selectedCategory];
+        })));
+
+
+
+
+        let xData = vis.filteredIndex.map(function (d) {
+            return d.year;
+        });
+
+        let yData = vis.filteredIndex.map(function (d) {
+            return d[selectedCategory];
+        });
+
+
+        vis.top5line = vis.svg.append("path")
+            .datum(vis.filteredIndex);
+
+        vis.top5line.enter()
+            .append()
+            .merge(vis.top5line)
+            .attr("fill", "none")
+            .attr("stroke", "salmon")
+            .attr("stroke-width", 1.5)
+            .transition().duration(3000).delay(500)
+            .attr("d", d3.line()
+                .x(function(d) { return vis.x(d.year)})
+                .y(function(d) { return vis.y(d[selectedCategory]) }))
+
+
+
+
+        vis.top5line.exit().remove();
+
+        // Update y-axis
+        vis.svg.select(".y-axis")
+            .transition()
+            .duration(2000)
+            .call(vis.yAxis);
+
+        // Update x-axis
+        vis.svg.select(".x-axis").call(vis.xAxis);
+
+
+    }
+
+}*/
