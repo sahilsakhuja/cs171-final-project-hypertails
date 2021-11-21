ccb14a6d87e52d98b97b653adb4f3ea3d693cc7e
Sahil Sakhuja <sahilsakhuja85@gmail.com>
2021-11-21 10:58:56 +0100

    feat: UI updated with Andrew's full page code

---
+++ js/fullpage.js
@@ -0,0 +1,12 @@
+
+// fullpage customization
+$('#fullpage').fullpage({
+    sectionsColor: ['#43aa8b', '#277da1', '#F2AE72', '#ff8fa3', '#4d908e','#f3722c','#f9c74f','#43aa8b','#577590', '#ff8fa3'],
+    sectionSelector: '.vertical-scrolling',
+    slideSelector: '.horizontal-scrolling',
+    navigation: true,
+    slidesNavigation: true,
+    controlArrows: false,
+    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection','sixthSection','seventhSection','eigthSection'],
+    menu: '#menu',
+});
