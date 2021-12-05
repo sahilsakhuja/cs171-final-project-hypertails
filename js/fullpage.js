5ad68cd45399feb7761804334e5386e843f250f6
Sahil Sakhuja <sahilsakhuja85@gmail.com>
2021-12-05 16:23:18 +0100

    fix: last section udpated and sahil pic added

--- js/fullpage.js
+++ js/fullpage.js
@@ -2,13 +2,13 @@
 // fullpage customization
 $('#fullpage').fullpage({
     // sectionsColor: ['#43aa8b', '#277da1', '#F2AE72', '#ff8fa3', '#4d908e','#f3722c','#f9c74f','#43aa8b','#577590', '#ff8fa3'],
-    sectionsColor: ['#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF','#FFFFFF'],
+    sectionsColor: ['#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF','#FFFFFF','#FFFFFF'],
     sectionSelector: '.vertical-scrolling',
     slideSelector: '.horizontal-scrolling',
     navigation: true,
     slidesNavigation: true,
     controlArrows: false,
-    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection','sixthSection'],
+    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection','sixthSection', 'seventhSection'],
     afterLoad: function( section, origin, destination, direction){
         if ((section === 'secondSection') && !animatedBarChartRun) {
             setTimeout(function() {
